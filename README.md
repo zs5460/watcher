ce93d8059f79c0fb201e4c83d6034274a3e77c8f
zs5460 <zs5460@gmail.com>
2023-08-08 13:53:08 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # watcher
+
+Package watcher implements a simple file monitor that calls a callback function when a file is modified, typically used to automatically load configuration files.
+
+## simple
+
+```go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/zs5460/jmc"
+	"github.com/zs5460/watcher"
+)
+
+type config struct {
+	Listen  string
+	Version string
+}
+
+var cfg config
+
+func main() {
+	w, err := watcher.New("config.json", loadConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Start()
+
+	<-make(chan struct{})
+}
+
+func loadConfig() {
+	jmc.MustLoadConfig("config.json", &cfg)
+	fmt.Printf("%#v\n", cfg)
+}
+
+```
+
+## License
+
+Released under MIT license, see [LICENSE](LICENSE) for details.
---
+++ watcher.go
@@ -0,0 +1,39 @@
+// Package watcher implements a simple file monitor that calls a callback
+// function when a file is modified, typically used to automatically load
+// configuration files
+package watcher
+
+import (
+	"os"
+	"time"
+)
+
+type watcher struct {
+	filename string
+	onChange func()
+}
+
+func New(fn string, call func()) (*watcher, error) {
+	_, err := os.Stat(fn)
+	if err != nil {
+		return nil, err
+	}
+	return &watcher{filename: fn, onChange: call}, nil
+}
+
+func (wc *watcher) watch() {
+	info, _ := os.Stat(wc.filename)
+	lastModTime := info.ModTime()
+	for range time.Tick(1 * time.Second) {
+		info, _ := os.Stat(wc.filename)
+		if !info.ModTime().After(lastModTime) {
+			continue
+		}
+		lastModTime = info.ModTime()
+		wc.onChange()
+	}
+}
+
+func (wc *watcher) Start() {
+	go wc.watch()
+}
